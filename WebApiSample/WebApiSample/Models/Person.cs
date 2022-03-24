9fa228b340fc44d7bc7810e72fca0afc9f8df69b
ANACA\gurdev.sandhu <gurdevs@xello.world>
2022-03-23 23:21:21 -0400

    Updated Person model

--- WebApiSample/WebApiSample/Models/Person.cs
+++ WebApiSample/WebApiSample/Models/Person.cs
@@ -9,6 +9,7 @@ namespace WebApiSample.Models
     public class Person
     {
         [Key]
+        public int Id { get; set; }
         public string Phone { get; set; }
         public string Name { get; set; }
     }
